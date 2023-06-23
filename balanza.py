5c90301cd27818d85625bf58db633fe40db22a5c
tomasberretta <tomas.berretta@ing.austral.edu.ar>
2023-06-23 17:08:30 -0300

    Fix balanza.py
    Co-authored-by: manumasjoan <manumasjoan@gmail.com>
    Co-authored-by: flor-resoagli <fresoagli@mulesoft.com>

--- balanza.py
+++ balanza.py
@@ -1,62 +1,66 @@
-import serial, time
+import time
 
-# ConfiguraciÃ³n de la comunicaciÃ³n serial con Arduino
-port = '/dev/cu.usbmodem1101'  # Cambiar al puerto adecuado // este es el de mas lejos de la pantalla
-baudrate = 9600  # Must match the baud rate in the Arduino sketch
-timeout = 10  # Timeout for serial communication (in seconds)
+import serial
 
-article_identifier = ['C', 'M', 'G']  # 1 ,2, 3
-# Open the serial port
-ser = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+# port = '/dev/cu.usbmodem11101'  # Make sure this is the correct port for your Arduino
+port = 'COM9'
+baudrate = 9600
+timeout = 10
 
-# Create an empty array to store the values
-sensor_values = []
+# Open serial connection to Arduino
+try:
+    ser = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+except serial.SerialException as e:
+    print(f"Error opening serial port: {e}")
+    exit()
 
-# Pedir al usuario que ingrese el identificador y el número de clavos solicitados
-input_value = input("Ingrese el tipo y la cantidad de clavos (por ejemplo: C 25) o S para frenar la medición: ")
-
-# Separar la cadena de entrada en dos partes
-input_parts = input_value.split()  # Usar un espacio como separador
-input_article = input_parts[0]  # La primera parte es el identificador
-input_number = input_parts[1]  # La segunda parte es el número
-
-while input_value != 'S':
-    # # Read a line from the serial port
-    # line = ser.readline().decode().strip()
-
-    # Convertir el número en un entero
-    number = int(input_number)
-
-    # Convertir el identificador y el número en una cadena de bytes
-    output_value =   str(
-        number)+ " " + str(input_article) + '\n'  # Convertir el identificador y el número en una cadena y agregar el fin de línea
-    output_bytes = output_value.encode()  # Codificar la cadena en bytes
-
-    # Escribir los bytes al puerto serial
-    ser.write(output_bytes)  # Enviar los bytes al puerto serial
-
-    # Parse the line as an integer value
-    try:
-        # sensorValue = int(line)
-        # print('Numero de clavos:', sensorValue)
-        #
-        # # Append the value to the array
-        # sensor_values.append(sensorValue)
-
-        # Escribir un valor al puerto serial
-        ser.write(output_bytes)  # Enviar 25 como clavos solicitados
-
-    except ValueError:
-        # print('Invalid data received:', line)
-        print('Invalid data received:')
-
-    # Pedir al usuario que ingrese el identificador y el número de clavos solicitados
+while True:
+    # Get user input
     input_value = input("Ingrese el tipo y la cantidad de clavos (por ejemplo: C 25) o S para frenar la medición: ")
 
-    # Separar la cadena de entrada en dos partes
-    input_parts = input_value.split()  # Usar un espacio como separador
-    input_article = input_parts[0]  # La primera parte es el identificador
-    input_number = input_parts[1]  # La segunda parte es el número
+    # Exit loop if user inputs 'S'
+    if input_value.upper() == 'S':
+        break
 
-# Close the serial port
+    # Split input into parts
+    input_parts = input_value.split()
+
+    # Check if the input format is correct
+    if len(input_parts) != 2:
+        print("Invalid input format. Please follow the format (e.g., C 25).")
+        continue
+
+    input_article, input_number = input_parts
+
+    # Check if the number part is an integer
+    try:
+        number = int(input_number)
+    except ValueError:
+        print('Invalid number received.')
+        continue
+
+    # Build output string and encode to bytes
+    a = input("Enter \"Instruction: C 25\"")
+    output_value = a + '\n'
+    output_bytes = output_value.encode()
+
+    # Send data through serial port
+    ser.write(output_bytes)
+    print(f"Sent to Arduino: {output_value.strip()}")
+    time.sleep(5)
+    print("Waiting for Arduino to be ready...")
+
+    response_serial = ""
+    while response_serial != "OK":
+        try:
+            response_serial = ser.readline().decode().strip()
+        except Exception as e:
+            pass
+        print(response_serial)
+        if response_serial != "OK":
+            print("Waiting for Arduino to be ready...")
+    time.sleep(5)
+    print(f"Got: {output_value}")
+
+# Close serial connection
 ser.close()
